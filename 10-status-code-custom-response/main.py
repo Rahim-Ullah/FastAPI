f4b98770d0ac8a51af1bc4629a10c4c6870c7242
Rahim-Ullah <rahim5ullah5@gmail.com>
2026-09-16 11:55:31 +0500

    status code-exception handling

--- 10-status-code-custom-response/main.py
+++ 10-status-code-custom-response/main.py
@@ -1,25 +1,53 @@
-from fastapi import FastAPI, status as status_code, HTTPException
+from fastapi import FastAPI, status as status_codes, HTTPException as http_exception
 
 app = FastAPI()
 
 
+class UserNotFoundException(Exception):
+    def __init__(self, user_name: str): # we can create our own exceptions and raise them using raise kw, and the exception is initialize by constructor and we can pass the user_id to it
+        self.user_name = user_name
 
-@app.post("/create_user", status_code=status_code.HTTP_201_CREATED) # we have a bunch of status codes in fastapi, we can use them to return the appropriate status code for our endpoints
+@app.post("/create_user", status_code=status_codes.HTTP_201_CREATED) # we have a bunch of status codes in fastapi, we can use them to return the appropriate status code for our endpoints
 async def create_user():
     return {"message": "User created successfully"}
 
 
 # custom response
-@app.get("/get_users", status_code=status_code.HTTP_200_OK)
+@app.get("/get_users", status_code=status_codes.HTTP_200_OK)
 async def get_users():
-    return {"message": "Users retrieved successfully", "status_code": status_code.HTTP_200_OK, }
+    return {"message": "Users retrieved successfully", "status_code": status_codes.HTTP_200_OK, }
 
 # get root with id
-@app.get("/get_users/{user_id}")
+# @app.get("/get_users/{user_id}")
+# async def get_user(user_id: int):
+#     if user_id < 1:
+#         raise http_exception(status_code=status_code.HTTP_400_BAD_REQUEST, detail="Invalid user ID Passed!") # the code inside raised will not be shown to user instead the details of the error will be
+#     return {"message": f"User with ID {user_id} retrieved successfully", "status_code": status_code.HTTP_200_OK}
+
+
+# Errors handling code custom, exceptions and global error handlers 
+@app.get("/get_users/{user_id}", status_code=status_codes.HTTP_404_NOT_FOUND)
 async def get_user(user_id: int):
     if user_id < 1:
-        raise HTTPException(status_code=status_code.HTTP_400_BAD_REQUEST, detail="Invalid user ID Passed!") # the code inside raised will not be shown to user instead the details of the error will be
-    return {"message": f"User with ID {user_id} retrieved successfully", "status_code": status_code.HTTP_200_OK}
+        raise http_exception(status_code=400, detail="Invalid user ID Passed!")
+    return {"message": f"User with ID {user_id} retrieved successfully", "status_code": status_codes.HTTP_200_OK}
+
+
+# Custom error handler [for that creating a model for the error is a good practice]
+# @app.exception_handler(UserNotFoundException)
+# async def user_not_found_exception_handler(request, exc):
+#     return {"message": f"User with name {exc.user_name} not found", "status_code": status_codes.HTTP_404_NOT_FOUND}
+
+    
+@app.get("/get_users/{user_name}")
+async def get_user(user_name: str):
+    # try:
+    if user_name != "rahim":
+        raise UserNotFoundException(user_name)
+    return {"message": f"User with name {user_name} retrieved successfully", "status_code": status_codes.HTTP_200_OK}
+
+
+
 
 
 if __name__ == "__main__":
